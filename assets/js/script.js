47190309b972b577e58c5d7f4eb5081f540e311c
Mara <mara.bermudez@gmail.com>
2021-03-20 03:06:25 -0400

    modified form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,25 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+var createTaskHandler = function (event) {
+  event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-var createTaskHandler = function(event) {
-   event.preventDefault();
+  // create list item
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
 
-   var listItemEl = document.createElement("li");
-   listItemEl.className = "task-item";
-   listItemEl.textContent = "This is a new task.";
-   tasksToDoEl.appendChild(listItemEl);
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+
+  // add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
+  tasksToDoEl.appendChild(listItemEl);
 };
-
-console.log(event);
 
 formEl.addEventListener("submit", createTaskHandler);
